b795b08b811e3db14b6b7544f1858fc81ccafe28
hridhi <57177761+hridhi@users.noreply.github.com>
2020-10-17 21:20:29 +0530

    incomplete

---
+++ min_jumps.py
@@ -0,0 +1,4 @@
+#Given an array of integers where each element represents the max number of steps that can be made forward from that element. 
+#The task is to find the minimum number of jumps to reach the end of the array (starting from the first element). 
+#If an element is 0, then cannot move through that element.
+
