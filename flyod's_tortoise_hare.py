8ac09f0fce737ee3a6446c7f620245acbaff36db
hridhi <57177761+hridhi@users.noreply.github.com>
2020-10-18 08:35:21 +0530

    array

---
+++ flyod's_tortoise_hare.py
@@ -0,0 +1,24 @@
+# finding the duplicates in an array which contains n+1 integers 
+# the naive approach will be checking each and every integer and this will take over O(n^2) complexity 
+# we can sort the array and find the duplicate 
+#else you can make the numbers in the array as keys of the dictionary and then whose ever keys value is 2 that is the duplicate 
+
+#else we can use floyd's tortoise and hare cycle detection algorithm for the following problem 
+#this helps to detect the cycle by using 2 pointers tortoise and hare and where hare moves faster than tortoise
+def floyd_tor_hare(a):
+  tor=hare=a[0]
+  #finding the intersection point which will be inside the loop  
+  while True:
+    tor=a[tor]
+    hare=a[a[hare]]
+    if tor==hare:
+      break
+  tor=a[0]
+  #finding the entrance of the loop 
+  while tor!=hare: 
+    tor=a[tor]
+    hare=a[hare]
+  return tor
+
+a=[2,5,9,6,9,3,8,9,7,1]
+print(floyd_tor_hare(a))
