df1a0c27f3d03bbf47a8a1b1cd10604d52ec40f2
hridhi <57177761+hridhi@users.noreply.github.com>
2020-10-18 23:24:51 +0530

    incomplete

---
+++ inversion_count.py
@@ -0,0 +1,8 @@
+#given an array we need to find its inversion count 
+#inversion count indicates that how far is the array from being sorted 
+#brute force method will be Traverse through the array and for every index find the number of smaller elements on its right side of the array. 
+#Sum up the counts for all index in the array and print the sum.
+#complexity will be O(n^2)
+
+#enhance merge sort 
+#
