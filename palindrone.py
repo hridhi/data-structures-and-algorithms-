35e1530325e8fd5b9de02c9d7b706ce0e52f4081
hridhi <57177761+hridhi@users.noreply.github.com>
2020-10-25 10:38:04 +0530

    doubt

---
+++ palindrone.py
@@ -0,0 +1,3 @@
+#Given a string, you keep swapping any two characters in the string randomly till the string becomes a palindrome. What is the expected number of swaps you will make? 
+#There will always be at least one palindrome which can be formed with the letters of the given string.
+#https://www.hackerrank.com/challenges/palindromes/editorial
