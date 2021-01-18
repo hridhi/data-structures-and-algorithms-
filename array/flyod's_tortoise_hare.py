f91fe8c6d374be7b4fe595e240153961aa79b13e
hridhi <57177761+hridhi@users.noreply.github.com>
2021-01-18 08:16:13 +0530

    Update flyod's_tortoise_hare.py

--- array/flyod's_tortoise_hare.py
+++ array/flyod's_tortoise_hare.py
@@ -2,6 +2,7 @@
 # the naive approach will be checking each and every integer and this will take over O(n^2) complexity 
 # we can sort the array and find the duplicate 
 #else you can make the numbers in the array as keys of the dictionary and then whose ever keys value is 2 that is the duplicate 
+#to get linear time complexity the elements should be stored in data structure so we can store it in a set , so if an element already in set then return it 
 
 #else we can use floyd's tortoise and hare cycle detection algorithm for the following problem 
 #this helps to detect the cycle by using 2 pointers tortoise and hare and where hare moves faster than tortoise
