12e8d0e68756d5a2ed602410655ce26d7c30401c
hridhi <57177761+hridhi@users.noreply.github.com>
2021-03-04 20:24:54 +0530

    Delete subarray_sum_0.py

--- subarray_sum_0.py
+++
@@ -1,6 +0,0 @@
-#we will be given an array of both positive , negative and zero numbers in the array 
-#approach one is we can check linearly if the sum or the number is less than the previous sum then change the sum 
-# if the sum becomes 0 at some point then return TRUE 
-#approach 2 
-#We can also use hashing. The idea is to iterate through the array and for every element arr[i], calculate sum of elements form 0 to i
-#if the sums repeat then return TRUE as the array will have a sum comtaining 0 
