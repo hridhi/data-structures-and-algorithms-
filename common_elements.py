ebcaf12ec38e613e4e7789062937f857c5bef3ba
hridhi <57177761+hridhi@users.noreply.github.com>
2021-03-04 20:25:50 +0530

    Delete common_elements.py

--- common_elements.py
+++
@@ -1,4 +0,0 @@
-#given 3 sorted arrays find the common elements in them 
-#we can check common elements in any 2 arrays and then we can check if that element is there in array 3 
-# this approach will take O(n^2) complexity
-#else we can make a dictionary of the 3 arrays and where every the value of a particular key is 3 we require those elements 
