6d95d9d4eb0dfd6656d3f6c7d0cff8f5df03659d
hridhi <57177761+hridhi@users.noreply.github.com>
2021-03-16 11:06:43 +0530

    Create Binomial_coefficient.py

---
+++ dynamic_programming/Binomial_coefficient.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nCr(self, n, r):
+        # code here
+        mod=int(10e9+7)
+        if (r>n):
+            return 0
+        dp=[[1 for i in range(1)]for j in range(n)]
+        for i in range(0,n):
+            for j in range(1,i+2):
+                if j==i+1:
+                    dp[i].append(1)
+                else:
+                    ans=(dp[i-1][j-1]%mod+dp[i-1][j]%mod)%mod
+                    dp[i].append(ans)
+        return dp[n-1][r]
