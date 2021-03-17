9e672efd9a35b5eb8b6379bcc818104814c34ec8
hridhi <57177761+hridhi@users.noreply.github.com>
2021-03-17 07:51:56 +0530

    Update intersection_Ll.py

--- linkedlist/intersection_Ll.py
+++ linkedlist/intersection_Ll.py
@@ -14,5 +14,31 @@ class Solution:
             b=headA if b==None else b.next
         return b
             
-                    
+#time limit exceeding code 
+# brute force approach 
+        
+def intersetPoint(head1,head2):
+    #code here
+    count=0
+    count1=0
+    req=0
+    while head1.next!=None:
+        count+=1
+    while head2.next!=None:
+        count1+=1
+    if count>count1:
+        req=1
+    else:
+        req=2
+    if req==1:
+        for i in range(abs(count=count1)):
+            head1=head1.next
+    else:
+        for i in range(abs(count=count1)):
+            head2=head2.next
+    for i in range(min(count,count1)):
+        if head1.data==head2.data:
+            return head1.data
+        head1=head1.next
+        head2=head2.next
         
