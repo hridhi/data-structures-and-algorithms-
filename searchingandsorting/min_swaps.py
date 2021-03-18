494b363f43530fc2d8022205ccd0d8a8d8963404
hridhi <57177761+hridhi@users.noreply.github.com>
2021-03-18 22:52:53 +0530

    Create min_swaps.py

---
+++ searchingandsorting/min_swaps.py
@@ -0,0 +1,22 @@
+class Solution:
+    #selection sort O(n^2)
+    #graph solution O(nLogn)
+	def minSwaps(self, arr):
+		#Code here
+		n = len(arr)
+        arrpos = [*enumerate(arr)]
+        arrpos.sort(key = lambda it : it[1])
+        vis = {k : False for k in range(n)}
+        ans = 0
+        for i in range(n):
+            if vis[i] or arrpos[i][0] == i:
+                continue
+            cycle_size = 0
+            j = i
+            while not vis[j]:
+                vis[j] = True
+                j = arrpos[j][0]
+                cycle_size += 1
+            if cycle_size > 0:
+                ans += (cycle_size - 1)
+        return ans
