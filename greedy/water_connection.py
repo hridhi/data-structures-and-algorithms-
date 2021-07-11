30c0ffa331f652926f3da8d43f2c8a2657f0fb77
hridhi <57177761+hridhi@users.noreply.github.com>
2021-07-11 12:07:10 +0530

    Create water_connection.py

---
+++ greedy/water_connection.py
@@ -0,0 +1,5 @@
+#Every house in the colony has at most one pipe going into it and at most one pipe going out of it. 
+#Tanks and taps are to be installed in a manner such that every house with one outgoing pipe but no incoming pipe gets a tank installed on its roof 
+#and every house with only an incoming pipe and no outgoing pipe gets a tap. 
+#Find the efficient way for the construction of the network of pipes.
+
